aaf378ff088fe59af156a709972012552b69e8b4
Luxilo <asiacuisine.re@gmail.com>
2025-11-20 13:50:25 +0400

    Fix: débogage des champs manquants

--- script.js
+++ script.js
@@ -608,7 +608,8 @@ function initializeSubscriptionForm() {
         const formData = new FormData(subscriptionForm);
         const data = Object.fromEntries(formData.entries());
 
-        console.log('Submitting data to /create-subscription:', data); // DEBUG: Log the data being sent
+        console.log('Submitting data with keys:', Object.keys(data)); // Specific debug
+        console.log('Value of "formula" being sent:', data.formula);     // Specific debug
 
         try {
             const response = await fetch('/create-subscription', {
